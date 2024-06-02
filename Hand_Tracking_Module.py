96f7b36d06ed78c617b1d86904270f49c5891838
kanca <kancain00@gmail.com>
2024-06-02 17:03:53 +0500

    comment

--- Hand_Tracking_Module.py
+++ Hand_Tracking_Module.py
@@ -36,7 +36,7 @@ class HandDetector():
             for id, lm in enumerate(myHand.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
+                #print(id, cx, cy)
                 lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy,), 15, (255, 0, 255), cv2.FILLED)
